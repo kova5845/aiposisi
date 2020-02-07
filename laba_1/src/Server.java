6dc3843dadc86c98f56e5df9b74263f957980e7d
alexey <lesha.kovalenko.1999@mail.ru>
2020-02-07 21:31:08 +0300

    first commit

---
+++ laba_1/src/Server.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private int port;
+    private  String directory;
+
+    private Server(int port, String directory) {
+        this.port = port;
+        this.directory = directory;
+    }
+
+    private void start(){
+        try (ServerSocket server = new ServerSocket(this.port)){
+            while(true) {
+                Socket socket = server.accept();
+                Thread thread = new Handler(socket, this.directory);
+                thread.start();
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        var port = Integer.parseInt(args[0]);
+        var directory = args[1];
+        new Server(port, directory).start();
+    }
+}
